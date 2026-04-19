afa9c5ab02379d3d07bfdce6ea59127a2545074a
yokochichichi <j2300171@gunma-u.ac.jp>
2026-04-19 17:38:40 +0900

    first commit

---
+++ src/test/java/memoapp/MemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package memoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
